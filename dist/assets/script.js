69180adb1339a1977b151f0dae5716a80674aae6
Gotaeyoon <taeyoongo007@gmail.com>
2022-03-26 03:36:48 +0900

    레이아웃 구조 진행중

--- dist/assets/script.js
+++ dist/assets/script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+$(document).ready(function () {
+  var lnb = $("._header").offset().top;
+  $(window).scroll(function () {
+    var window = $(this).scrollTop();
+    console.log(window);
+
+    if (lnb < window) {
+      $("._header").css({
+        "position": "fixed",
+        "background-color": "rgba(37, 37, 37,.8)",
+        "border-bottom": "none"
+      });
+    } else {
+      $("._header").css({
+        "position": "absoulte",
+        "background-color": "transparent",
+        "border-bottom": "1px solid #fff"
+      });
+    }
+  });
+});
