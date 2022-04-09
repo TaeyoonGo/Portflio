f05a8e7e95e7c93fad0aa459b96a463d15c9235a
Gotaeyoon <taeyoongo007@gmail.com>
2022-04-09 20:33:14 +0900

    자바스크립트 함수형으로 정리

--- dist/assets/script.js
+++ dist/assets/script.js
@@ -1,134 +1,156 @@
 "use strict";
 
-// 헤더 스크롤 이벤트
 $(document).ready(function () {
-  var lnb = $("._header").offset().top;
-  $(window).scroll(function () {
-    var window = $(this).scrollTop();
+  // 헤더 스크롤 이벤트
+  function Scroll() {
+    var lnb = $("._header").offset().top;
+    $(window).scroll(function () {
+      var window = $(this).scrollTop();
 
-    if (lnb < window) {
-      $("._header").css({
-        "position": "fixed",
-        "background-color": "rgba(37, 37, 37,.8)",
-        "border-bottom": "none"
-      });
-    } else {
-      $("._header").css({
-        "position": "absoulte",
-        "background-color": "transparent",
-        "border-bottom": "1px solid #fff"
-      });
-    }
-  });
-}); // emailjs 실행
-
-$(document).ready(function () {
-  emailjs.init("U4dPLe9cyju41MkJl");
-  $('input[name=submit]').click(function () {
-    var templateParams = {
-      name: $('input[name=name]').val(),
-      phone: $('input[name=phone]').val(),
-      email: $('input[name=email]').val(),
-      message: $('textarea[name=message]').val()
-    };
-    emailjs.send('service_69v2pcn', 'template_hp6y3wm', templateParams).then(function (response) {
-      console.log('SUCCESS!', response.status, response.text);
-      alert('안녕하세요 고태윤입니다 :) 연락을 주셔서 감사합니다.');
-    }, function (error) {
-      console.log('FAILED...', error);
+      if (lnb < window) {
+        $("._header").css({
+          "position": "fixed",
+          "background-color": "rgba(37, 37, 37,.8)",
+          "border-bottom": "none"
+        });
+      } else {
+        $("._header").css({
+          "position": "absoulte",
+          "background-color": "transparent",
+          "border-bottom": "1px solid #fff"
+        });
+      }
     });
-  });
-}); // 네비게이션 이동
+  } // Email
 
-$(document).ready(function () {
-  $('._home-btn').click(function () {
-    var offset = $('._home-section').offset();
-    $('html,body').animate({
-      scrollTop: offset.top - 100
-    }, 400);
-  });
-  $('._about-btn').click(function () {
-    var offset = $('._about-section').offset();
-    $('html,body').animate({
-      scrollTop: offset.top - 100
-    }, 400);
-  });
-  $('._portflio-btn').click(function () {
-    var offset = $('._portflio-section').offset();
-    $('html,body').animate({
-      scrollTop: offset.top - 100
-    }, 400);
-  });
-  $('._skill-btn').click(function () {
-    var offset = $('._skill-section').offset();
-    $('html,body').animate({
-      scrollTop: offset.top - 100
-    }, 400);
-  });
-  $('._contact-btn').click(function () {
-    var offset = $('._contact-section').offset();
-    $('html,body').animate({
-      scrollTop: offset.top - 100
-    }, 400);
-  });
-}); // Top 버튼
 
-$(document).ready(function () {
-  $('.btn-totop').click(function () {
-    $('html,body').animate({
-      scrollTop: 0
-    }, {
-      duration: 400
+  function Email() {
+    emailjs.init("U4dPLe9cyju41MkJl");
+    $('input[name=submit]').click(function () {
+      var templateParams = {
+        name: $('input[name=name]').val(),
+        phone: $('input[name=phone]').val(),
+        email: $('input[name=email]').val(),
+        message: $('textarea[name=message]').val()
+      };
+      emailjs.send('service_69v2pcn', 'template_hp6y3wm', templateParams).then(function (response) {
+        console.log('SUCCESS!', response.status, response.text);
+        alert('안녕하세요 고태윤입니다 :) 연락을 주셔서 감사합니다.');
+      }, function (error) {
+        console.log('FAILED...', error);
+      });
     });
-    return false;
-  });
-  $(window).on('scroll', function () {
-    var scrollTop = $(window).scrollTop();
+  } // 네비게이션 이동
 
-    if (scrollTop > 150) {
-      $('.btn-totop').css({
-        'opacity': 1
+
+  function Navi() {
+    $('._home-btn').click(function () {
+      var offset = $('._home-section').offset();
+      $('html,body').animate({
+        scrollTop: offset.top - 100
+      }, 400);
+    });
+    $('._about-btn').click(function () {
+      var offset = $('._about-section').offset();
+      $('html,body').animate({
+        scrollTop: offset.top - 100
+      }, 400);
+    });
+    $('._portflio-btn').click(function () {
+      var offset = $('._portflio-section').offset();
+      $('html,body').animate({
+        scrollTop: offset.top - 100
+      }, 400);
+    });
+    $('._skill-btn').click(function () {
+      var offset = $('._skill-section').offset();
+      $('html,body').animate({
+        scrollTop: offset.top - 100
+      }, 400);
+    });
+    $('._contact-btn').click(function () {
+      var offset = $('._contact-section').offset();
+      $('html,body').animate({
+        scrollTop: offset.top - 100
+      }, 400);
+    });
+  } // Top 버튼
+
+
+  function Top() {
+    $('.btn-totop').click(function () {
+      $('html,body').animate({
+        scrollTop: 0
+      }, {
+        duration: 400
       });
-    } else {
-      $('.btn-totop').css({
-        'opacity': 0
-      });
-    }
-  });
-}); // 모바일 네비게이션 애니메이션 동작
+      return false;
+    });
+    $(window).on('scroll', function () {
+      var scrollTop = $(window).scrollTop();
 
-$(document).ready(function () {
-  $('._menu-bar').click(function () {
-    $('.line').removeClass('init');
-    $('._line-top').toggleClass('line-top').toggleClass('top-reverse');
-    $('._line-mid').toggleClass('line-mid').toggleClass('mid-reverse');
-    $('._line-bot').toggleClass('line-bot').toggleClass('bot-reverse');
-  });
-}); // 모바일 네비게이션 실행
+      if (scrollTop > 150) {
+        $('.btn-totop').css({
+          'opacity': 1
+        });
+      } else {
+        $('.btn-totop').css({
+          'opacity': 0
+        });
+      }
+    });
+  } // 모바일 네비게이션 애니메이션 동작
 
-$(document).ready(function () {
-  $('._menu-bar').click(function () {
-    $(this).toggleClass('active');
-    $(this).siblings('._navis').toggleClass('active');
-  });
-}); // 네비게이션 바 닫힘
 
-$(document).ready(function () {
-  $('._navi').click(function () {
-    $(this).parent('._navis').removeClass('active');
-    $(this).parent('._navis').siblings('._menu-bar').find('.line').removeClass('init');
-    $(this).parent('._navis').siblings('._menu-bar').find('._line-top').toggleClass('line-top').toggleClass('top-reverse');
-    $(this).parent('._navis').siblings('._menu-bar').find('._line-mid').toggleClass('line-mid').toggleClass('mid-reverse');
-    $(this).parent('._navis').siblings('._menu-bar').find('._line-bot').toggleClass('line-bot').toggleClass('bot-reverse');
-    console.log();
-  });
-}); //massory
+  function MobileAnimation() {
+    $('._menu-bar').click(function () {
+      $('.line').removeClass('init');
+      $('._line-top').toggleClass('line-top').toggleClass('top-reverse');
+      $('._line-mid').toggleClass('line-mid').toggleClass('mid-reverse');
+      $('._line-bot').toggleClass('line-bot').toggleClass('bot-reverse');
+    });
+  } // 모바일 네비게이션 실행
 
-$(document).ready(function () {
-  $('.grid').masonry({
-    columnWidth: '._grid-item',
-    itemSelector: '._grid-item',
-    gutter: 20,
-    percentPosition: true
-  });
+
+  function MobileNavi() {
+    $('._menu-bar').click(function () {
+      $(this).toggleClass('active');
+      $(this).siblings('._navis').toggleClass('active');
+    });
+  } // 네비게이션 바 닫힘
+
+
+  function NaviClose() {
+    $('._navi').click(function () {
+      $(this).parent('._navis').removeClass('active');
+      $(this).parent('._navis').siblings('._menu-bar').find('.line').removeClass('init');
+      $(this).parent('._navis').siblings('._menu-bar').find('._line-top').toggleClass('line-top').toggleClass('top-reverse');
+      $(this).parent('._navis').siblings('._menu-bar').find('._line-mid').toggleClass('line-mid').toggleClass('mid-reverse');
+      $(this).parent('._navis').siblings('._menu-bar').find('._line-bot').toggleClass('line-bot').toggleClass('bot-reverse');
+      console.log();
+    });
+  } //massory
+
+
+  function Massory() {
+    $('.grid').masonry({
+      columnWidth: '._grid-item',
+      itemSelector: '._grid-item',
+      gutter: 20,
+      percentPosition: true
+    });
+  }
+
+  function init() {
+    Scroll();
+    Email();
+    Navi();
+    Top();
+    MobileAnimation();
+    MobileNavi();
+    NaviClose();
+    Massory();
+  }
+
+  init();
 });
